ca79d13d915511841b494c7a284b6a876f1ee72a
JossElito179 <118587500+JossElito179@users.noreply.github.com>
2024-02-09 23:52:39 +0300

    Just a little fix in Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,36 @@
-import { useContext } from 'react';
-import { ThemeProvider } from 'styled-components';
-import ThemeContext from './contexts/ThemeContext';
+import { useEffect } from 'react';
+import {
+  Routes,
+  Route,
+  useLocation
+} from 'react-router-dom';
+// import { ThemeProvider } from 'styled-components';
+// import ThemeContext from './contexts/ThemeContext';
 import LandingPage from './pages/LandingPage';
-import VoirAnnonces from './pages/VoirAnnonces';
-import ChatBox from './components/type/ChatBox';
-import GlobalStyles from './styles/GlobalStyles';
-import VoirChat from './pages/VoirChat';
+import Login from './Login';
+// import VoirAnnonces from './pages/VoirAnnonces';
+// import ChatBox from './components/type/ChatBox';
+// import GlobalStyles from './styles/GlobalStyles';
+// import VoirChat from './pages/VoirChat';
 
 function App() {
-  const { theme } = useContext(ThemeContext);
+  // const { theme } = useContext(ThemeContext);
+  const location = useLocation();
+
+  useEffect(() => {
+    document.querySelector('html').style.scrollBehavior = 'auto'
+    window.scroll({ top: 0 })
+    document.querySelector('html').style.scrollBehavior = ''
+  }, [location.pathname]); // triggered on route change
+
 
   return (
     <>
-      <ThemeProvider theme={{ theme }}>
-        <GlobalStyles />
-        {/* <VoirChat/> */}
-        {/* <LandingPage /> */}
-        <VoirAnnonces />
-      </ThemeProvider>
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/home" element={<LandingPage />} ></Route>
+      </Routes>
     </>
   );
 }
